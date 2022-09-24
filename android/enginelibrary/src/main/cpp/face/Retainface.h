a76be1c088a3464559fb0841d3f3499bc10352f1
xiuyu0000 <huxiuyu1943@sina.com>
2022-09-24 19:31:03 +0800

    Add Vision

--- android/enginelibrary/src/main/cpp/face/Retainface.h
+++ android/enginelibrary/src/main/cpp/face/Retainface.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022 Huawei Technologies Co., Ltd
+ * Copyright 2022
  * Sort of Code from https://github.com/OpenFirework/Retinaface_CPP.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
